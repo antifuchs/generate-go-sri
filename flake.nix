734979e2fc445bf4c9f9c3b733c7e8d12ce79141
Andreas Fuchs <asf@boinkor.net>
2024-09-17 19:11:45 -0400

    Don't use flake-parts here
    It only provides a very minor benefit and can easily be replaced with
    a manual approach. That should address
    https://github.com/hercules-ci/flake-parts/issues/252.

--- flake.nix
+++ flake.nix
@@ -2,51 +2,56 @@
   description = "Regenerate go module vendorHashes from inputs.";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.tailscale.url = "github:tailscale/tailscale";
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} ({
-      withSystem,
-      flake-parts-lib,
+  outputs = {
+    self,
+    nixpkgs,
+    tailscale,
+  }: let
+    systems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "riscv64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+    eachSystem = f:
+      nixpkgs.lib.genAttrs systems (
+        system:
+          f rec {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
+            devshell = import ./. {nixpkgs = pkgs;};
+          }
+      );
+  in {
+    formatter = eachSystem ({pkgs, ...}: pkgs.alejandra);
+
+    packages = eachSystem ({
+      system,
+      pkgs,
       ...
-    }: let
-      inherit (flake-parts-lib) importApply;
-    in {
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+    }: {
+      default = self.packages.${system}.nardump;
+      nardump = pkgs.buildGo123Module rec {
+        pname = "nardump";
+        version = tailscale.rev;
+        src = "${tailscale}/cmd/nardump";
+        vendorHash = null;
 
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        ...
-      }: {
-        formatter = pkgs.alejandra;
-
-        packages = {
-          default = config.packages.nardump;
-          nardump = pkgs.buildGo123Module rec {
-            pname = "nardump";
-            version = inputs.tailscale.rev;
-            src = "${inputs.tailscale}/cmd/nardump";
-            vendorHash = null;
-
-            # Fake up a go module inside the nardump dir, so we don't
-            # have to download & cache the entirety of tailscale's
-            # deps:
-            prePatch = ''
-              cat >go.mod <<EOF
-              module github.com/tailscale/tailscale/cmd/nardump
-              go 1.23.0
-              require ()
-              EOF
-            '';
-          };
-        };
-      };
-      flake = {
-        flakeModules.default = ./flake-module.nix;
+        # Fake up a go module inside the nardump dir, so we don't
+        # have to download & cache the entirety of tailscale's
+        # deps:
+        prePatch = ''
+          cat >go.mod <<EOF
+          module github.com/tailscale/tailscale/cmd/nardump
+          go 1.23.0
+          require ()
+          EOF
+        '';
       };
     });
+    flakeModules.default = ./flake-module.nix;
+  };
 }
